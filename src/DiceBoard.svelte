39783e55e29457c8a850450f1209fa59fa9daf48
Andrey Dyugaev <deprimehell@gmail.com>
2023-10-16 20:19:13 +0500

    feat: change cube edges

--- src/DiceBoard.svelte
+++ src/DiceBoard.svelte
@@ -28,6 +28,7 @@
   let preloading = true;
   let rolling = false;
   let rotation = false;
+  let edge = 1;
 
   const maxWidth = 380;
   const width = (window.innerWidth > maxWidth)
@@ -79,25 +80,40 @@
   camera.add(light2);
   scene.add(light2);
 
+  let basePosition;
+  const edgeSet = {
+    1: {x: 1.1, y: -0.1, z: 0.8},
+    2: {x: -0.8, y: -3, z: 0.2},
+    3: {x: 1, y: 0.3, z: -0.3},
+    4: {x: 4.2, y: 2.9, z: -0.4},
+    5: {x: 2.8, y: 0, z: 1.3},
+    6: {x: -0.65, y: 0.8, z: -3.6},
+    7: {x: -1, y: 3.4, z: -2.9},
+    8: {x: 0, y: 2.5, z: 0.8},
+    9: {x: 2.4, y: 2.4, z: 3},
+    10: {x: 0.1, y: -3.1, z: 1.1},
+    11: {x: 3.8, y: 1.3, z: 0.6},
+    12: {x: 0.18, y: -2.5, z: 0.25},
+    13: {x: 3.9, y: 0.28, z: 0.45},
+    14: {x: 0.2, y: -0, z: 3.2},
+    15: {x: -6.3, y: 3.8, z: -2.9},
+    16: {x: 2.4, y: 3.0, z: 0.9},
+    17: {x: -2.8, y: 4.1, z: -3.8},
+    18: {x: -5.2, y: 5.7, z: -3.5},
+    19: {x: 0, y: 0, z: 0},
+    20: {x: -2, y: 3.2, z: 0.8},
+  }
 
   // Grid
   scene.add(new GridHelper(5, 30));
-
   const loader = new GLTFLoader();
-  let rotated = false;
 
   // Lifecycle
   const render = () => {
     requestAnimationFrame(render);
     if (mixer) {
       const delta = clock.getDelta();
-
       mixer.update(delta);
-      // if (mixer.time >= 4 && !rotated) {
-      //   sceneModel.rotateX(5.3)
-      //   sceneModel.rotateZ(5.3)
-      //   rotated = true;
-      // }
     }
     renderer.render(scene, camera);
   };
@@ -110,12 +126,28 @@
       return;
     }
     rotation = false;
+    setEdge();
     prepareAnimation();
     render();
   };
 
+  const setEdge = () => {
+    const group = sceneModel.children[0];
+    basePosition = !basePosition
+      ? group.children[0].quaternion.clone()
+      : basePosition;
+    group.children[0].quaternion.copy(basePosition);
+    edge = Math.ceil(Math.random() * (20 - 1) + 1);
+    const set  = edgeSet[edge];
+    console.log(edge)
+    group.children[0].rotateX(set.x);
+    group.children[0].rotateY(set.y);
+    group.children[0].rotateZ(set.z);
+  }
+
   const prepareAnimation = () => {
     const time =  9.3;
+    // const time =  1;
     rolling = true;
     mixer = new AnimationMixer(sceneModel);
     const anim = rollAnimation[1];
@@ -126,7 +158,6 @@
     setTimeout(() => {rolling = false}, (time * 1000) - 500);
   };
 
-
   onMount(() => {
     // renderer.render(scene, camera);
     const diceBoard = document.getElementById('dice-board');
@@ -134,47 +165,15 @@
       diceBoard.appendChild(renderer.domElement);
 
       //  TODO: define device to choose model size
-      loader.load('/glb-models/dice5.glb', function (gltf) {
-        // console.log(gltf);
-
+      loader.load('/glb-models/dice6.glb', function (gltf) {
         preloading    = false;
         rollAnimation = gltf.animations;
         sceneModel    = gltf.scene;
-
-        // const scale = 0.016
-        const scale = 1.001
-        // sceneModel.children[0].scale.set(scale, scale, scale);
-        // rollAnimation.tracks[2].values = rollAnimation.tracks[2].values.map(el => scale);
+        const scale = 1.001;
 
         sceneModel.position.set(0, 0, 0.6);
-        // sceneModel.children[0].rotateX(1);
-        // sceneModel.children[0].rotateY(1);
-
-        const relDice = sceneModel.children[0];
-
         const baseDice = sceneModel.children[1];
-
-        sceneModel.children = [
-          baseDice
-        ];
-
-        const group = sceneModel.children[0];
-        // let rot = 0
-        // let rotY = 1.2
-        // let rotZ = 0.5
-        // group.children[0].rotateX(rot);
-        // group.children[0].rotateY(rotY);
-        // group.children[0].rotateZ(rotZ);
-        // group.children[0].scale.set(2, 2, 2);
-        // group.children[1].scale.set(scale, scale, scale);
-        // console.log(group.children)
-        // sceneModel.children[1].visible = false;
-        // sceneModel.traverse(function (object) {
-        //   if (object.isMesh) {
-        //     object.castShadow = true; // shadow
-        //     object.receiveShadow = true; // Accept the shadow of others
-        //   }
-        // });
+        sceneModel.children = [baseDice];
 
         scene.add(sceneModel);
         renderer.render(scene, camera);
@@ -194,7 +193,7 @@
     on:click={onRollDice}
   >
     <span class="primary-button-inner inline-block font-semibold w-48 py-2.5 rounded-2xl">
-      {rolling ? "rolling..." : "ROLL THE DICE"}
+      {rolling ? `edge ${edge}` : "ROLL THE DICE"}
     </span>
   </button>
 </footer>
